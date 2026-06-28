51cd6c1c4bc38c49bd8f854dc0f25913a9f6020e
Hamid Aboutalebkadkhodaeian <habootaleb92@gmail.com>
2026-06-28 11:10:54 -0400

    Create detect_onh.py

---
+++ src/detect_onh.py
@@ -0,0 +1,15 @@
+"""
+ONH Detection Module
+
+Author: Hamid Aboutaleb Kadkhodaeian
+
+Objective:
+Detect the optic nerve head (ONH) center from retinal
+en-face OCT images.
+
+Future improvements:
+- Adaptive thresholding
+- Morphological filtering
+- Circularity analysis
+- Deep-learning segmentation
+"""
